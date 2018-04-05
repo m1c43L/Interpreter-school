d55756987271f4eff26797a57fd8d679b74d12d6
Michael Gilbert <gilbertmichael888@gmail.com>
2018-04-05 11:28:14 -0700

    Working FunctionEnvironmentRecord for debugger.

--- src/interpreter/debugger/FunctionEnvironmentRecord.java
+++ src/interpreter/debugger/FunctionEnvironmentRecord.java
@@ -1,5 +1,5 @@
 
-package interpreter;
+package interpreter.debugger;
 
 /**
  * MileStone 2.
@@ -10,7 +10,7 @@ public class FunctionEnvironmentRecord {
     
     private Table symbols;
     private String funcName;
-    private int startingLineNo, endingLineNo, currentLineNo;
+    private Integer startingLineNo, endingLineNo, currentLineNo;
     
     public FunctionEnvironmentRecord(){
         symbols = new Table();
@@ -26,58 +26,61 @@ public class FunctionEnvironmentRecord {
         endingLineNo = end;
     }
     
-    public void getOffset(){
-        
-    }
-    
     public void setCurrentLineNumber(int newLineNo){
         currentLineNo = newLineNo;
     }
     
-    public void put(Symbol var, int val){  
+    public void put(String var, int val){  
         symbols.put(var, val);
     }
     
-    public void pop(int lines){
-        while(lines > 0){
-            symbols.pop();
-            lines --;
-        }
+    public void pop(int numToPop){
+        symbols.endScope(numToPop);
     }
     
+    /**
+     * prints debug
+     */
     public void dump(){
         StringBuilder builder = new StringBuilder();
         builder.append("(<");
         if(!symbols.keys().isEmpty()){
-        for(Symbol s: symbols.keys()){
-            builder.append(s.getId()+"/"+symbols.get(s));
+        for(String s: symbols.keys()){
+            builder.append(s).append("/").append(symbols.get(s));
             builder.append(",");
         }
         builder.setCharAt( builder.length() - 1,'>');
         }else builder.append(">");
         
-        builder.append("," + funcName).append("," + startingLineNo)
-                .append("," + endingLineNo).append("," + currentLineNo);
-        builder.append(")");
+        builder.append(",").append((funcName == null)? "-": funcName).append(",")
+                .append((startingLineNo == null)? "-": startingLineNo)
+                .append(",")
+                .append((endingLineNo == null)? "-": endingLineNo)
+                .append(",")
+                .append((currentLineNo == null)? "-" : currentLineNo)
+                .append(")");
+        
         System.out.println(builder);
     }
     
-    
+    /** 
+    *   test the class
+    */
     public static void main(String args[]){
         FunctionEnvironmentRecord t = new FunctionEnvironmentRecord();
         t.beginScope();
         t.dump();
-        t.setFunctionInfo("g", 1, 2);
+        t.setFunctionInfo("g", 1, 20);
         t.dump();
         t.setCurrentLineNumber(5);
         t.dump();
-        t.put(new Symbol("a"),4);
+        t.put("a",4);
         t.dump();
-        t.put(new Symbol("b"), 2);
+        t.put("b", 2);
         t.dump();
-        t.put(new Symbol("c"), 7);
+        t.put("c", 7);
         t.dump();
-        t.put(new Symbol("a"), 1);
+        t.put("a", 1);
         t.dump();
         t.pop(2);
         t.dump();
@@ -91,36 +94,23 @@ public class FunctionEnvironmentRecord {
 class Binder{
     
     private Object value;
-    private Symbol prevtop;   // prior symbol in same scope
+    private String prevtop;   // prior symbol in same scope
     private Binder tail;      // prior binder for same symbol
                             // restore this when closing scope
-    Binder(Object v, Symbol p, Binder t) {
+    Binder(Object v, String p, Binder t) {
 	value=v; prevtop=p; tail=t;
     }
 
     Object getValue() { return value; }
-    Symbol getPrevtop() { return prevtop; }
+    String getPrevtop() { return prevtop; }
     Binder getTail() { return tail; }
     }
 
-class Symbol{
-    
-    private String id;
-    
-    Symbol(String newId){
-        id = newId;
-    }
-    
-    public String getId(){
-        return id;
-    }
-    
-}
 
  class Table {
 
-  private java.util.HashMap<Symbol,Binder> symbols = new java.util.HashMap<Symbol,Binder>();
-  private Symbol top;    // reference to last symbol added to
+  private java.util.HashMap<String,Binder> symbols = new java.util.HashMap<String,Binder>();
+  private String top;    // reference to last symbol added to
                          // current scope; this essentially is the
                          // start of a linked list of symbols in scope
   private Binder marks;  // scope mark; essentially we have a stack of
@@ -133,7 +123,7 @@ class Symbol{
  /**
   * Gets the object associated with the specified symbol in the Table.
   */
-  public Object get(Symbol key) {
+  public Object get(String key) {
 	Binder e = symbols.get(key);
 	return e.getValue();
   }
@@ -143,7 +133,7 @@ class Symbol{
   * Maintain the list of symbols in the current scope (top);<br>
   * Add to list of symbols in prior scope with the same string identifier
   */
-  public void put(Symbol key, Object value) {
+  public void put(String key, Object value) {
 	symbols.put(key, new Binder(value, top, symbols.get(key)));
 	top = key;
   }
@@ -160,25 +150,21 @@ class Symbol{
   * Restores the table to what it was at the most recent beginScope
   *	that has not already been ended.
   */
-  public void endScope() {
-	while (top!=null) {
+  public void endScope(int numToPop) {
+      
+	while (numToPop > 0) {
 	   Binder e = symbols.get(top);
 	   if (e.getTail()!=null) symbols.put(top,e.getTail());
 	   else symbols.remove(top);
 	   top = e.getPrevtop();
+           numToPop--;
 	}
-	top=marks.getPrevtop();
-	marks=marks.getTail();
-  }
-  
-  public void pop(){
-      Binder e = symbols.get(top);
-      symbols.remove(top);
-      top = e.getPrevtop();   
+	//top=marks.getPrevtop();
+	//marks=marks.getTail();
   }
 
   /**
    * @return a set of the Table's symbols.
    */
-  public java.util.Set<Symbol> keys() {return symbols.keySet();}
+  public java.util.Set<String> keys() {return symbols.keySet();}
 }
